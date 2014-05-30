622c790b6134b0de6fdda2e2e696944de1830c6c
Jeremy Katras <jkatras@fullsail.edu>
2014-05-30 12:12:32 -0400

    cleanup/comment old; new test code (controller)

--- controllers/config.js
+++ controllers/config.js
@@ -1,143 +1,126 @@
 //App's main module with dependencies for UI-Router and FireBase
-var EatInApp = angular.module('EatInApp', ["ui.router", "firebase"]);
+//var EatInApp = angular.module('EatInApp', ["ui.router", "firebase"]);
 //Configures which states will activate which views and url's and what 
 //their controllers will be.
-EatInApp.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/search");
-	$stateProvider
+//EatInApp.config(function($stateProvider, $urlRouterProvider) {
+//	$urlRouterProvider.otherwise("/search");
+//	$stateProvider
 //	search state is default view
-	.state('search', {
-		url: '/search',
-		templateUrl: 'views/search.html'
-//		controller: function ($scope) {
-//			$scope.keyword = [];
-//			if ($scope.include = '') {
-//				$scope.include = []
-//			};
-//			if ($scope.exclude = '') {
-//				$scope.exclude = []
-//			};
-//			$scope.keyword = [];
-//			$scope.include = '';
-//			$scope.exclude = '';
-//		}
-	})
+//	.state('search', {
+//		url: '/search',
+//		templateUrl: 'views/search.html'
+//	})
 // navigates to About view
-	.state('about', {
-		url: '/about',
-		templateUrl: 'views/about.html'
-	})
+//	.state('about', {
+//		url: '/about',
+//		templateUrl: 'views/about.html'
+//	})
 // navigates to Login view/form; not currently functional.
-	.state('login', {
-		url: '/login',
-		templateUrl: 'views/login.html'
-	})
+//	.state('login', {
+//		url: '/login',
+//		templateUrl: 'views/login.html'
+//	})
 //	This controller will allow creation of user profile through
 //	signup view and data going to FireBase. Not currently implemented.
-	.state('signup', {
-		url: '/signup',
-		templateUrl: 'views/signup.html',
+//	.state('signup', {
+//		url: '/signup',
+//		templateUrl: 'views/signup.html',
 // When activated, will link to EatIn FireBase and push profile details to unique array
-		controller: function ($scope, angularFireCollection) {
-			var ref = new Firebase("https://eatin-base.firebaseio.com/");
-			$scope.profile = angularFireCollection(ref);
-			$scope.createProfile = function () {
-				$scope.profile.add({uname: $scope.username, pass: $scope.password, email: $scope.email, fname: $scope.firstname, lname: $scope.lastname});
-			}
-		}
-	})
+//		controller: function ($scope, angularFireCollection) {
+//			var ref = new Firebase("https://eatin-base.firebaseio.com/");
+//			$scope.profile = angularFireCollection(ref);
+//			$scope.createProfile = function () {
+//				$scope.profile.add({uname: $scope.username, pass: $scope.password, email: $scope.email, fname: $scope.firstname, lname: $scope.lastname});
+//			}
+//		}
+//	})
 //	Displays logged-in user's profile. Also not implemented.
-	.state('profile', {
-		url: '/profile',
-		templateUrl: 'views/profile.html',
-		controller: function ($scope, angularFireCollection) {
-			var ref = new Firebase("https://eatin-base.firebaseio.com/");
-			$scope.profile = angularFireCollection(ref);
-		}
-	})
+//	.state('profile', {
+//		url: '/profile',
+//		templateUrl: 'views/profile.html',
+//		controller: function ($scope, angularFireCollection) {
+//			var ref = new Firebase("https://eatin-base.firebaseio.com/");
+//			$scope.profile = angularFireCollection(ref);
+//		}
+//	})
 //	Results view is nested inside search view.
 //  Controller makes call to Yummly API
-	.state('search.results', {
-		url: '/results',
-		templateUrl: 'views/search.results.html',
-		controller: function ($scope, $http) {
-		 	//var for unique App ID
-		 	$scope.appId = '4606347e';
-		 	//var for unique API Key
-		    $scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
-		    //array that will contain search results
-		    $scope.results = [];
-	     	//results returned will be JSONP format
-	     	
-//			ORIGINAL CODE
-
+//	.state('search.results', {
+//		url: '/results',
+//		templateUrl: 'views/search.results.html',
+//		controller: function ($scope, $http) {
+//		 	var for unique App ID
+//		 	$scope.appId = '4606347e';
+//		 	var for unique API Key
+//		    $scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
+//		    array that will contain search results
+//		    $scope.results = [];
+//	     	results returned will be JSONP format
+//			
 //	       	$http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + 
 //	       		$scope.appId + '&_app_key=' + $scope.apiKey + 
-//	       		'&q=' + $scope.keyword +
-//	       		'&noUserSettings=true' + 
-//	       		'&allowedIngredient[]=' + $scope.include + 
+//	       		'&q=' + $scope.keyword + 
+//	       		'&allowedIngredient[]=' + $scope.include +
 //	       		'&excludedIngredient[]=' + $scope.exclude + 
 //	       		'&requirePictures=true&callback=JSON_CALLBACK').
-
-//---------------------TEST CODE-----------------------
-
-// *** PROBLEM WITH API CALL RESULTS FROM blank field for allowedIngredient and/or q.
-// If parameters removed from API call, then search will go through.
-// Can't have either 'undefined'?
-// Need to have conditional which excludes parameters if field is blank
-			
-//			if ($scope.include = '') {
-//				$scope.include = [];
-//			};
-//			if ($scope.exclude = '') {
-//				$scope.exclude = [];
-//			};
-			
-	       	$http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + 
-	       		$scope.appId + '&_app_key=' + $scope.apiKey + 
-	       		'&q=' + $scope.keyword +
-//	       		'&noUserSettings=true' + 
-	       		'&allowedIngredient[]=' + $scope.include +
-	       		'&excludedIngredient[]=' + $scope.exclude + 
-	       		'&requirePictures=true&callback=JSON_CALLBACK').
-
-//---------------------END TEST------------------------
-	       		
-        	//if successful return, parse data
-	        success(function(data) {
-	        	//forEach loop runs through matches
-	        	//push names to results array
-	        	angular.forEach(data.matches, function(recipe, index) {
-	        		$scope.results.push(recipe);
-	    		});
-	    		console.log(data);
-	        }). // success
-	    	error(function(error) {
-				alert('Please check your search terms and try again');
-			}); // error
-		} 
-	})
+//
+//        	if successful return, parse data
+//	        success(function(data) {
+//	        	forEach loop runs through matches
+//	        	push names to results array
+//	        	angular.forEach(data.matches, function(recipe, index) {
+//	        		$scope.results.push(recipe);
+//	    		});
+//	    		console.log(data);
+//	        }).  success
+//	    	error(function(error) {
+//				alert('Please check your search terms and try again');
+//			});  error
+//		} 
+//	})
 //  Details view replaces main search view
 //  Another call is made to API	
-	.state('details', {
-		//unique recipe ID fetched from results of clicked item
-		url: '/details/:id',
-		templateUrl: 'views/details.html',
-		controller: function ($scope, $http, $stateParams) {
-			//includes recipe id in url parameters
-			$scope.id = $stateParams.id;
-			$scope.appId = '4606347e';
-    		$scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
-	    	$http.jsonp('http://api.yummly.com/v1/api/recipe/' +$stateParams.id
-	    	+ '?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&callback=JSON_CALLBACK').
-	    	success(function(data) {
-	    		$scope.details = data;
-	    		console.log($scope.details);
-	    	}). //success
-	    	error(function(error) {
-				alert('Recipe not found');
-			}); // error
-			
-	   	}
-	})
+//	.state('details', {
+//		unique recipe ID fetched from results of clicked item
+//		url: '/details/:id',
+//		templateUrl: 'views/details.html',
+//		controller: function ($scope, $http, $stateParams) {
+//			includes recipe id in url parameters
+//			$scope.id = $stateParams.id;
+//			$scope.appId = '4606347e';
+//    		$scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
+//	    	$http.jsonp('http://api.yummly.com/v1/api/recipe/' +$stateParams.id
+//	    	+ '?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&callback=JSON_CALLBACK').
+//	    	success(function(data) {
+//	    		$scope.details = data;
+//	    		console.log($scope.details);
+//	    	}). success
+//	    	error(function(error) {
+//				alert('Recipe not found');
+//			});  error	
+//	   	}
+//	})
+//});
+
+//--------------------------TEST CODE----------------------------------
+var app = angular.module('EatInApp', []);
+
+app.controller('ListCtrl', function ($scope, $http) {
+	$scope.appId = '4606347e';
+	$scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
+	$scope.results = [];
+	$http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + 
+   		$scope.appId + '&_app_key=' + $scope.apiKey + 
+   		'&q=' + $scope.keyword + 
+   		'&allowedIngredient[]=' + $scope.include +
+   		'&excludedIngredient[]=' + $scope.exclude + 
+   		'&requirePictures=true&callback=JSON_CALLBACK').
+    success(function(data) {
+    	angular.forEach(data.matches, function(recipe, index) {
+    		$scope.results.push(recipe);
+		});
+    }).
+	error(function(error) {
+		alert('Please check your search terms and try again');
+	});
 });
